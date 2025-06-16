1e4973d036424c03a9feadcdd683c180901d91e0
bernardofla10 <167584225+bernardofla10@users.noreply.github.com>
2025-06-15 21:51:01 -0300

    Add files via upload

---
+++ trabalho2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
